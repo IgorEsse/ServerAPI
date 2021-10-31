0abe03672f61e3c99c168ac45467172b637f5346
IgorEsse <esseigor@gmail.com>
2021-11-01 02:23:32 +0400

    Create template Spring Application - Hello World

---
+++ src/test/java/ru/esseigor/serverAPI/ServerApi1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.esseigor.serverAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApi1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
